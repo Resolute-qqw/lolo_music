396c2b41b6b8c30aa6ef51461a615d1e90f0404c
李浩淇(EX-LIHAOQI001) <1425679428@qq.com>
2020-09-15 17:47:12 +0800

    添加mixin

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 // import router from './router'
 import store from './store'
+import fastClick from 'fastclick'
+
+fastClick.attach(document.body)
 
 Vue.config.productionTip = false
 
